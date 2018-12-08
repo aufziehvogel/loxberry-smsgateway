64cc278401ef2a853ebf31fbe9798966f62d337e
Stefan Koch <programming@stefan-koch.name>
2018-12-08 12:56:31 +0100

    fix service setup bugs

--- postroot.sh
+++ postroot.sh
@@ -4,9 +4,10 @@ cp /opt/loxberry/data/plugins/$pluginfoldername/supervisor.conf /etc/supervisor/
 cp /opt/loxberry/data/plugins/$pluginfoldername/supervisorsend.conf /etc/supervisor/conf.d/smsgatewaysend.conf
 cp /opt/loxberry/data/plugins/$pluginfoldername/smsd.conf /etc/smsd.conf
 
-/etc/init.d/smstools enable
+service smstools enable
 /etc/init.d/smstools start
 
 supervisorctl reread
+supervisorctl reload
 supervisorctl start smsgateway
 supervisorctl start smsgatewaysend
